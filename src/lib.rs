85bca75261e4c7933f974a3051945e4e237f5c89
Lawrence Neal <nealla@lwneal.com>
2019-06-12 19:59:38 -0700

    Actually run ffmpeg!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -6,6 +8,13 @@ mod tests {
     }
 }
 
-pub fn foo(input_filename: &str) {
-    println!("param: {:?}", input_filename);
+pub fn foo(input_filename: &str, output_filename: &str) {
+    //println!("Run the following ffmpeg -i {:?} -vc libx264 -pix_fmt yuvj420p {:?}", input_filename, output_filename);
+    //let cmd = format!("ffmpeg -i {:?} -vc libx264 -pix_fmt yuvj420p {:?}", input_filename, output_filename);
+    let cmd_output = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(input_filename)
+        .arg(output_filename)
+        .output()
+        .expect("FFmpeg call failed");
 }
